a7c34c0adac74ab6fb649610a5b65d6959741cc8
binky-stack <65432587+binky-stack@users.noreply.github.com>
2021-01-21 19:11:45 -0500

    Update and rename contact.html to contact-us.php

--- contact-us.php
+++ contact-us.php
@@ -18,15 +18,48 @@
 </nav>
 
 
+<?php
+    $error = false;
+    $sent = false;
+
+    if(isset($_POST['submit'])) {
+        if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['comments'])) {
+            $error = true;
+        }
+        else {
+            $to = "linardsberzins@gmail.com";
+
+            $name = trim($_POST['name']);
+            $email = trim($_POST['email']);
+            $comments = trim($_POST['comments']);
+
+            $subject = "Contact Form";
+
+            $message =  "Name: $name \r\n Email: $email \r\n Comments: $comments";
+            $headers = "From:" . $name;
+            $mailsent = mail($to, $subject, $message, $headers);
+
+            if($mailsent) {
+                $sent = true;
+            }
+        }
+    }
+?>
+
+<?php if($error == true){ ?>
+<p class="error">Text</p>
+<?php } if($sent == true) { ?>
+<p class="sent">Text</p>
+<?php } ?>
 <div id="form">
     <form name="contact" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
         <fieldset>
-            <h4>Contact Us!</h4>
+            <h4>Contact Me!</h4>
             <label for="name">Name:</label>
                 <input type="text" name="name" id="name"/>
                 <label for="email"/>Email:</label>
                 <input type="text" name="email" id="email"/>
-                <label for="comments" id="comments">Message:</label>
+                <label for="comments" id="comments">Comments:</label>
                 <textarea name="comments" id=""></textarea>
                 <fieldset>
                 <input class="btn" type="submit" name="submit"  class="submit" value="Send email"/>
@@ -35,5 +68,6 @@
         </fieldset>
     </form>
 </div>
+
 </body>
 </html>
